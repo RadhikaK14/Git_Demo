49ddfbd1c632cd5dc02d1520d7a19214fd62a333
RadhikaK14 <62436185+RadhikaK14@users.noreply.github.com>
2021-08-09 22:00:40 +0100

    Add more files

--- Github4.py
+++ Github4.py
@@ -1,1 +1,16 @@
-print('abc' + '123')
+
+
+language = 'Python'
+print('I am learning {0}'.format(language))
+
+
+a = 5 // 2
+print(a)
+
+print(type(123.45))
+
+print('abc' + '3')
+
+print('Hello')
+print('World')
+
